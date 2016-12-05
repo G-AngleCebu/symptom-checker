d9d1f6876ba83e00a40175e185df6f5c25ce7e03
davybolivar <davy.bolivar@gmail.com>
2016-12-05 14:10:06 +0800

    New bg

--- dev/js/background.js
+++ dev/js/background.js
@@ -1,113 +1,201 @@
-(function () {
-  let resizeReset = function() {
-  	w = canvasBody.width = window.innerWidth;
-  	h = canvasBody.height = window.innerHeight;
+(function() {
+  // RequestAnimFrame: a browser API for getting smooth animations
+  window.requestAnimFrame = (function(){
+    return  window.requestAnimationFrame       ||
+  		  window.webkitRequestAnimationFrame ||
+  		  window.mozRequestAnimationFrame    ||
+  		  window.oRequestAnimationFrame      ||
+  		  window.msRequestAnimationFrame     ||
+  		  function( callback ){
+  			window.setTimeout(callback, 1000 / 60);
+  		  };
+  })();
+
+  // Initializing the canvas
+  // I am using native JS here, but you can use jQuery,
+  // Mootools or anything you want
+  var canvas = document.getElementById("canvas");
+
+  // Initialize the context of the canvas
+  var ctx = canvas.getContext("2d");
+
+  // Set the canvas width and height to occupy full window
+  var W = window.innerWidth, H = window.innerHeight;
+  canvas.width = W;
+  canvas.height = H;
+
+  // Some variables for later use
+  var particleCount = 150,
+  	particles = [],
+  	minDist = 70,
+  	dist;
+
+  // Function to paint the canvas black
+  function paintCanvas() {
+  	// Set the fill color to black
+  	ctx.fillStyle = "rgba(0,0,0,1)";
+
+  	// This will create a rectangle of white color from the
+  	// top left (0,0) to the bottom right corner (W,H)
+  	ctx.fillRect(0,0,W,H);
   }
 
-  const opts = {
-  	particleColor: "rgb(255,255,255)",
-  	lineColor: "rgb(255,255,255)",
-  	particleAmount: 50,
-  	defaultSpeed: .05,
-  	variantSpeed: .3,
-  	defaultRadius: 2,
-  	variantRadius: 2,
-  	linkRadius: 300,
-  };
+  // Now the idea is to create some particles that will attract
+  // each other when they come close. We will set a minimum
+  // distance for it and also draw a line when they come
+  // close to each other.
 
-  window.addEventListener("resize", function(){
-  	deBouncer();
-  });
+  // The attraction can be done by increasing their velocity as
+  // they reach closer to each other
 
-  let deBouncer = function() {
-      clearTimeout(tid);
-      tid = setTimeout(function() {
-          resizeReset();
-      }, delay);
-  };
+  // Let's make a function that will act as a class for
+  // our particles.
 
-  let checkDistance = function(x1, y1, x2, y2){
-  	return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-  };
+  function Particle() {
+  	// Position them randomly on the canvas
+  	// Math.random() generates a random value between 0
+  	// and 1 so we will need to multiply that with the
+  	// canvas width and height.
+  	this.x = Math.random() * W;
+  	this.y = Math.random() * H;
 
-  let linkPoints = function(point1, hubs){
-  	for (let i = 0; i < hubs.length; i++) {
-  		let distance = checkDistance(point1.x, point1.y, hubs[i].x, hubs[i].y);
-  		let opacity = 1 - distance / opts.linkRadius;
-  		if (opacity > 0) {
-  			drawArea.lineWidth = 2;
-  			drawArea.strokeStyle = `rgba(${rgb[0]}, ${rgb[1]}, ${rgb[2]}, ${opacity})`;
-  			drawArea.beginPath();
-  			drawArea.moveTo(point1.x, point1.y);
-  			drawArea.lineTo(hubs[i].x, hubs[i].y);
-  			drawArea.closePath();
-  			drawArea.stroke();
+
+  	// We would also need some velocity for the particles
+  	// so that they can move freely across the space
+  	this.vx = -1 + Math.random() * 2;
+  	this.vy = -1 + Math.random() * 2;
+
+  	// Now the radius of the particles. I want all of
+  	// them to be equal in size so no Math.random() here..
+  	this.radius = 4;
+
+  	// This is the method that will draw the Particle on the
+  	// canvas. It is using the basic fillStyle, then we start
+  	// the path and after we use the `arc` function to
+  	// draw our circle. The `arc` function accepts four
+  	// parameters in which first two depicts the position
+  	// of the center point of our arc as x and y coordinates.
+  	// The third value is for radius, then start angle,
+  	// end angle and finally a boolean value which decides
+  	// whether the arc is to be drawn in counter clockwise or
+  	// in a clockwise direction. False for clockwise.
+  	this.draw = function() {
+  		ctx.fillStyle = "white";
+  		ctx.beginPath();
+  		ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+
+  		// Fill the color to the arc that we just created
+  		ctx.fill();
+  	}
+  }
+
+  // Time to push the particles into an array
+  for(var i = 0; i < particleCount; i++) {
+  	particles.push(new Particle());
+  }
+
+  // Function to draw everything on the canvas that we'll use when
+  // animating the whole scene.
+  function draw() {
+
+  	// Call the paintCanvas function here so that our canvas
+  	// will get re-painted in each next frame
+  	paintCanvas();
+
+  	// Call the function that will draw the balls using a loop
+  	for (var i = 0; i < particles.length; i++) {
+  		p = particles[i];
+  		p.draw();
+  	}
+
+  	//Finally call the update function
+  	update();
+  }
+
+  // Give every particle some life
+  function update() {
+
+  	// In this function, we are first going to update every
+  	// particle's position according to their velocities
+  	for (var i = 0; i < particles.length; i++) {
+  		p = particles[i];
+
+  		// Change the velocities
+  		p.x += p.vx;
+  		p.y += p.vy
+
+  		// We don't want to make the particles leave the
+  		// area, so just change their position when they
+  		// touch the walls of the window
+  		if(p.x + p.radius > W)
+  			p.x = p.radius;
+
+  		else if(p.x - p.radius < 0) {
+  			p.x = W - p.radius;
   		}
-  	}
-  }
 
-  Particle = function(xPos, yPos){
-  	this.x = Math.random() * w;
-  	this.y = Math.random() * h;
-  	this.speed = opts.defaultSpeed + Math.random() * opts.variantSpeed;
-  	this.directionAngle = Math.floor(Math.random() * 360);
-  	this.color = opts.particleColor;
-  	this.radius = opts.defaultRadius + Math.random() * opts. variantRadius;
-  	this.vector = {
-  		x: Math.cos(this.directionAngle) * this.speed,
-  		y: Math.sin(this.directionAngle) * this.speed
-  	};
-  	this.update = function(){
-  		this.border();
-  		this.x += this.vector.x;
-  		this.y += this.vector.y;
-  	};
-  	this.border = function(){
-  		if (this.x >= w || this.x <= 0) {
-  			this.vector.x *= -1;
+  		if(p.y + p.radius > H)
+  			p.y = p.radius;
+
+  		else if(p.y - p.radius < 0) {
+  			p.y = H - p.radius;
   		}
-  		if (this.y >= h || this.y <= 0) {
-  			this.vector.y *= -1;
+
+  		// Now we need to make them attract each other
+  		// so first, we'll check the distance between
+  		// them and compare it to the minDist we have
+  		// already set
+
+  		// We will need another loop so that each
+  		// particle can be compared to every other particle
+  		// except itself
+  		for(var j = i + 1; j < particles.length; j++) {
+  			p2 = particles[j];
+  			distance(p, p2);
   		}
-  		if (this.x > w) this.x = w;
-  		if (this.y > h) this.y = h;
-  		if (this.x < 0) this.x = 0;
-  		if (this.y < 0) this.y = 0;
-  	};
-  	this.draw = function(){
-  		drawArea.beginPath();
-  		drawArea.arc(this.x, this.y, this.radius, 0, Math.PI*2);
-  		drawArea.closePath();
-  		drawArea.fillStyle = this.color;
-  		drawArea.fill();
-  	};
-  };
 
-  function setup(){
-  	particles = [];
-  	resizeReset();
-  	for (let i = 0; i < opts.particleAmount; i++){
-  		particles.push( new Particle() );
-  	}
-  	window.requestAnimationFrame(loop);
-  }
-
-  function loop(){
-  	window.requestAnimationFrame(loop);
-  	drawArea.clearRect(0,0,w,h);
-  	for (let i = 0; i < particles.length; i++){
-  		particles[i].update();
-  		particles[i].draw();
-  	}
-  	for (let i = 0; i < particles.length; i++){
-  		linkPoints(particles[i], particles);
   	}
   }
 
-  const canvasBody = document.getElementById("c"),
-  drawArea = canvasBody.getContext("2d");
-  let delay = 200, tid,
-  rgb = opts.lineColor.match(/\d+/g);
-  resizeReset();
-  setup();
+  // Distance calculator between two particles
+  function distance(p1, p2) {
+  	var dist,
+  		dx = p1.x - p2.x;
+  		dy = p1.y - p2.y;
+
+  	dist = Math.sqrt(dx*dx + dy*dy);
+
+  	// Draw the line when distance is smaller
+  	// then the minimum distance
+  	if(dist <= minDist) {
+
+  		// Draw the line
+  		ctx.beginPath();
+  		ctx.strokeStyle = "rgba(255,255,255,"+ (1.2-dist/minDist) +")";
+  		ctx.moveTo(p1.x, p1.y);
+  		ctx.lineTo(p2.x, p2.y);
+  		ctx.stroke();
+  		ctx.closePath();
+
+  		// Some acceleration for the partcles
+  		// depending upon their distance
+  		var ax = dx/2000,
+  			ay = dy/2000;
+
+  		// Apply the acceleration on the particles
+  		p1.vx -= ax;
+  		p1.vy -= ay;
+
+  		p2.vx += ax;
+  		p2.vy += ay;
+  	}
+  }
+
+  // Start the main animation loop using requestAnimFrame
+  function animloop() {
+  	draw();
+  	requestAnimFrame(animloop);
+  }
+
+  animloop();
 })();
