02c5bf8ceef2a11db5f72174fe2464bb54f0b837
Alfonz Montelibano <m.alfonz@gmail.com>
2016-12-09 15:34:43 +0800

    Update diagnosis loop with UI design

--- dev/js/diagnosis.js
+++ dev/js/diagnosis.js
@@ -1,11 +1,11 @@
 var $ = require('jquery');
 var qi = require('./qi.js');
-var pages = require('./pageController.js');
 
 // cache dom
 var $diagnosisForm = $('form#dynamic-form');
 // var $submitButton = $diagnosisForm.find('button[type="submit"]');
-var $choices = $('#form-area');
+var $formArea = $('#form-area');
+var $questionArea = $diagnosisForm.find('.question-area');
 
 // current items data
 var symptomItems;
@@ -73,15 +73,13 @@ function submitSingleAnswer(e){
 
 // utility function to add new symptoms to choregraphe and run diagnosis
 function submitDiagnosisToChoregraphe(symptomListToDiagnose){
-	$choices.find('button').prop('disabled', true);
-	// $submitButton.prop('disabled', true);
+	$formArea.find('button').prop('disabled', true);
+	$formArea.html('');
 	qi.raiseEvent('diagnosis', JSON.stringify(symptomListToDiagnose));
 }
 
 // receives diagnosis result (see sample_data/sample diagnosis result.json)
 exports.receiveDiagnosisResult = function(data){
-	pages.goToPage("#dynamic-form");
-
 	var diagnosisResult = JSON.parse(data);
 	var question = diagnosisResult.question;
 	var questionItems = question.items;
@@ -90,35 +88,31 @@ exports.receiveDiagnosisResult = function(data){
 	symptomItems = questionItems;
 
 	// clear choices list
-	$choices.html('');
+	$formArea.html('');
 
-	$('#question').text(question.text);
-
-	// $diagnosisForm.find('h1').html(question.text);
-	// $diagnosisForm.find('h1').html(question.text + ' (' + question.type + ')');
-
-	// $submitButton.hide();
+	// set question text
+	$questionArea.html('');
+	$questionArea.append('<span id="question" class="element--fadeup">' + question.text + '</span>');
 
 	// GROUP_SINGLE
 	if(currentQuestionType == "group_single"){
 		questionItems.forEach(function(item){
-			// $choices.append('<button type="button" name="choice" value="' + item.id + '">' + item.name + '</button>');
-			$choices.append(
+			// $formArea.append('<button type="button" name="choice" value="' + item.id + '">' + item.name + '</button>');
+			$formArea.append(
 				'<input id="' + item.id + '" name="choice" type="radio" value="' + item.id + '"/>'+
 				'<div class="dynamic-input-wrapper element--fadedown">'+
 					'<label class="radio-label" for="' + item.id + '">'+
 						item.name+
 					'</label>'+
 				'</div>');
-			$choices.find('input[name="choice"]').on('click', submitGroupSingleAnswer);
+			$formArea.find('input[name="choice"]').on('click', submitGroupSingleAnswer);
 		});
-		$choices.find('button[name="choice"]').on('click', submitGroupSingleAnswer);
+		$formArea.find('button[name="choice"]').on('click', submitGroupSingleAnswer);
 	}
 	// GROUP_MULTIPLE
 	else if(currentQuestionType == "group_multiple"){
 		questionItems.forEach(function(item){
-			// $choices.append('<input type="checkbox" id="' + item.id + '" value="' + item.id +'" name="choice" /> <label for="' + item.id + '">' + item.name + '</label><br/>');
-			$choices.append(
+			$formArea.append(
 				'<input id="' + item.id + '" name="choice" type="checkbox" value="' + item.id + '"/>'+
 				'<div class="dynamic-input-wrapper element--fadedown">'+
 					'<label class="checkbox-label" for="' + item.id + '">'+
@@ -128,37 +122,30 @@ exports.receiveDiagnosisResult = function(data){
 				'</div>');
 			
 		});
-		$choices.append('<button type="submit">Submit</button>');
-
-		// $submitButton.prop('disabled', false);
-		// $submitButton.show();
+		$formArea.append('<button type="submit">Submit</button>');
 	}
 	// SINGLE
 	else if(currentQuestionType == "single"){
 		var questionChoices = questionItems[0].choices;
 		// button
-
-		// $choices.append('<button type="button" name="choice" value="present">Yes</button>');
-		// $choices.append('<button type="button" name="choice" value="absent">No</button>');
-		// $choices.append('<button type="button" name="choice" value="unknown">Don\'t know</button>');
-		$choices.append(
+		$formArea.append(
 			'<input id="yes-option" name="choice" type="radio" value="present"/>'+
 			'<div class="dynamic-input-wrapper element--fadedown">'+
-				'<label class="radio-label" for="yes-option">YES</label>'+
+				'<label class="radio-label" for="yes-option">Yes</label>'+
 			'</div>');
 
-		$choices.append(
+		$formArea.append(
 			'<input id="no-option" name="choice" type="radio" value="absent"/>'+
 			'<div class="dynamic-input-wrapper element--fadedown">'+
-				'<label class="radio-label" for="no-option">NO</label>'+
+				'<label class="radio-label" for="no-option">No</label>'+
 			'</div>');
 
-		$choices.append(
+		$formArea.append(
 			'<input id="dontknow-option" name="choice" type="radio" value="unknown"/>'+
 			'<div class="dynamic-input-wrapper element--fadedown">'+
-				'<label class="radio-label" for="dontknow-option">DON\'T KNOW</label>'+
+				'<label class="radio-label" for="dontknow-option">Don\'t know</label>'+
 			'</div>');
 
-		$choices.find('input[name="choice"]').on('click', submitSingleAnswer);
+		$formArea.find('input[name="choice"]').on('click', submitSingleAnswer);
 	}
 }
--- dev/js/userDetails.js
+++ dev/js/userDetails.js
@@ -1,12 +1,48 @@
+var $ = require('jquery');
+var qi = require('./qi.js');
+var pages = require('./pageController.js');
+
 exports.age = null;
 exports.gender = null;
 exports.weight = null;
 exports.height = null;
 
+var $ageGenderForm = $('#info-1-form');
+var $weightHeightForm = $('#info-2-form');
+
+var $form1SubmitButton = $ageGenderForm.find('button[type="submit"]');
+var $form2SubmitButton = $weightHeightForm.find('button[type="submit"]');
+
+var $ageInputField = $ageGenderForm.find('#age-input');
+
+var $radioFields = $ageGenderForm.find('.radio-field');
+var $maleGenderField = $radioFields.find('#male-option');
+var $femaleGenderField = $radioFields.find('#female-option');
+
+(function(){
+	$form1SubmitButton.on('click', function(){
+		var gender = $radioFields.find('input[name="gender-input"]:checked').val();
+
+		exports.age = $ageInputField.val();
+		exports.gender = gender;
+
+		qi.raiseEvent('setAgeGender', JSON.stringify({age: exports.age, gender: gender}));
+	});
+
+	$form2SubmitButton.on('click', function(){
+		pages.goToPage("#dynamic-form");
+		qi.raiseEvent('startDiagnosis');
+	});
+})();
+
 exports.setAge = function(age){
+	$ageInputField.val(age);
 	exports.age = JSON.stringify(age);
 }
 
 exports.setGender = function(gender){
+	$radioFields.find('input[name="gender-input"]').prop('checked', false);
+	$radioFields.find('input[value="' + gender + '"]').prop('checked', true);
+
 	exports.gender = JSON.stringify(gender);
 }
